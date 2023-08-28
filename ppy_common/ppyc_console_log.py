676bc5d06b458e62f11e1d57f216c3b71572bb0e
hmtmcse <hmtm.cse@gmail.com>
2023-08-28 10:22:28 +0600

    added system log system

--- ppy_common/ppyc_console_log.py
+++ ppy_common/ppyc_console_log.py
@@ -6,50 +6,50 @@ class Console:
     log_starting: str = ">>"
 
     @staticmethod
-    def print_log(text, color=None, bold=None):
-        if Console.enable_log:
+    def print_log(text, color=None, bold=None, system_log: bool = False):
+        if Console.enable_log or system_log:
             click.echo(click.style(text, fg=color, bold=bold))
 
     @staticmethod
-    def green(text, bold=False):
-        Console.print_log(text, color="green", bold=bold)
+    def green(text, bold=False, system_log: bool = False):
+        Console.print_log(text, color="green", bold=bold, system_log=system_log)
 
     @staticmethod
-    def blue(text, bold=False):
-        Console.print_log(text, color="blue", bold=bold)
+    def blue(text, bold=False, system_log: bool = False):
+        Console.print_log(text, color="blue", bold=bold, system_log=system_log)
 
     @staticmethod
-    def red(text, bold=False):
-        Console.print_log(text, color="red", bold=bold)
+    def red(text, bold=False, system_log: bool = False):
+        Console.print_log(text, color="red", bold=bold, system_log=system_log)
 
     @staticmethod
-    def yellow(text, bold=False):
-        Console.print_log(text, color="yellow", bold=bold)
+    def yellow(text, bold=False, system_log: bool = False):
+        Console.print_log(text, color="yellow", bold=bold, system_log=system_log)
 
     @staticmethod
-    def magenta(text, bold=False):
-        Console.print_log(text, color="magenta", bold=bold)
+    def magenta(text, bold=False, system_log: bool = False):
+        Console.print_log(text, color="magenta", bold=bold, system_log=system_log)
 
     @staticmethod
-    def cyan(text, bold=False):
-        Console.print_log(text, color="cyan", bold=bold)
+    def cyan(text, bold=False, system_log: bool = False):
+        Console.print_log(text, color="cyan", bold=bold, system_log=system_log)
 
     @staticmethod
     def get_message_format(text):
         return f"{Console.log_starting} {str(text)}"
 
     @staticmethod
-    def error(message):
-        return Console.red(Console.get_message_format(message), True)
+    def error(message, system_log: bool = False):
+        return Console.red(Console.get_message_format(message), True, system_log=system_log)
 
     @staticmethod
-    def success(message):
-        return Console.green(Console.get_message_format(message), True)
+    def success(message, system_log: bool = False):
+        return Console.green(Console.get_message_format(message), True, system_log=system_log)
 
     @staticmethod
-    def info(message):
-        return Console.yellow(Console.get_message_format(message), True)
+    def info(message, system_log: bool = False):
+        return Console.yellow(Console.get_message_format(message), True, system_log=system_log)
 
     @staticmethod
-    def log(message):
-        return Console.blue(Console.get_message_format(message), True)
+    def log(message, system_log: bool = False):
+        return Console.blue(Console.get_message_format(message), True, system_log=system_log)
