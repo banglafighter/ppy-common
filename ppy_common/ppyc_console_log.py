ec8ec29bcfc12285867cc843129f9522569bd038
hmtmcse <hmtm.cse@gmail.com>
2023-10-08 13:31:53 +0600

    added environment system

--- ppy_common/ppyc_console_log.py
+++ ppy_common/ppyc_console_log.py
@@ -12,50 +12,52 @@ class Console:
             click.echo(click.style(text, fg=color, bold=bold))
 
     @staticmethod
-    def green(text, bold=False, system_log: bool = False):
-        Console.print_log(text, color="green", bold=bold, system_log=system_log)
+    def green(text, bold=False, system_log: bool = False, enable_staring: bool = False):
+        Console.print_log(Console.get_message_format(text, enable_staring=enable_staring), color="green", bold=bold, system_log=system_log)
 
     @staticmethod
-    def blue(text, bold=False, system_log: bool = False):
-        Console.print_log(text, color="blue", bold=bold, system_log=system_log)
+    def blue(text, bold=False, system_log: bool = False, enable_staring: bool = False):
+        Console.print_log(Console.get_message_format(text, enable_staring=enable_staring), color="blue", bold=bold, system_log=system_log)
 
     @staticmethod
-    def red(text, bold=False, system_log: bool = False):
-        Console.print_log(text, color="red", bold=bold, system_log=system_log)
+    def red(text, bold=False, system_log: bool = False, enable_staring: bool = False):
+        Console.print_log(Console.get_message_format(text, enable_staring=enable_staring), color="red", bold=bold, system_log=system_log)
 
     @staticmethod
-    def yellow(text, bold=False, system_log: bool = False):
-        Console.print_log(text, color="yellow", bold=bold, system_log=system_log)
+    def yellow(text, bold=False, system_log: bool = False, enable_staring: bool = False):
+        Console.print_log(Console.get_message_format(text, enable_staring=enable_staring), color="yellow", bold=bold, system_log=system_log)
 
     @staticmethod
-    def magenta(text, bold=False, system_log: bool = False):
-        Console.print_log(text, color="magenta", bold=bold, system_log=system_log)
+    def magenta(text, bold=False, system_log: bool = False, enable_staring: bool = False):
+        Console.print_log(Console.get_message_format(text, enable_staring=enable_staring), color="magenta", bold=bold, system_log=system_log)
 
     @staticmethod
-    def cyan(text, bold=False, system_log: bool = False):
-        Console.print_log(text, color="cyan", bold=bold, system_log=system_log)
+    def cyan(text, bold=False, system_log: bool = False, enable_staring: bool = False):
+        Console.print_log(Console.get_message_format(text, enable_staring=enable_staring), color="cyan", bold=bold, system_log=system_log)
 
     @staticmethod
-    def get_message_format(text):
+    def get_message_format(text, enable_staring: bool = False):
+        if not enable_staring:
+            return text
         return f"{Console.log_starting} {str(text)}"
 
     @staticmethod
-    def error(message, system_log: bool = False):
-        return Console.red(Console.get_message_format(message), True, system_log=system_log)
+    def error(message, system_log: bool = False, enable_staring: bool = True):
+        return Console.red(message, True, system_log=system_log, enable_staring=enable_staring)
 
     @staticmethod
-    def success(message, system_log: bool = False):
-        return Console.green(Console.get_message_format(message), True, system_log=system_log)
+    def success(message, system_log: bool = False, enable_staring: bool = True):
+        return Console.green(message, True, system_log=system_log, enable_staring=enable_staring)
 
     @staticmethod
-    def info(message, system_log: bool = False):
-        return Console.yellow(Console.get_message_format(message), True, system_log=system_log)
+    def info(message, system_log: bool = False, enable_staring: bool = True):
+        return Console.yellow(message, True, system_log=system_log, enable_staring=enable_staring)
 
     @staticmethod
-    def log(message, system_log: bool = False):
-        return Console.blue(Console.get_message_format(message), True, system_log=system_log)
+    def log(message, system_log: bool = False, enable_staring: bool = True):
+        return Console.blue(message, True, system_log=system_log, enable_staring=enable_staring)
 
     @staticmethod
-    def run(command, home, env=None, **kwargs):
-        response = subprocess.run(command, shell=True, cwd=home, env=env, **kwargs)
+    def run(command, home, env=None):
+        response = subprocess.run(command, shell=True, cwd=home, env=env)
         return response
